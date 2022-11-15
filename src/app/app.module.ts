c2f29a69fbcc7d58de56a744de8e2fe4f3e06642
Lazynormz <skupi1234@gmail.com>
2022-11-15 20:07:39 +0100

    Added some very simple login / signup page(s) and a navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './user/signup/signup.component';
 import { UserModule } from './user/user.module';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
