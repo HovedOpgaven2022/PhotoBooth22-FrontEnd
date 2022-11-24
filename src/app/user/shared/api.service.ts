4e7999a5cefb54652b2e77c49059996e9372fc16
Lazynormz <skupi1234@gmail.com>
2022-11-24 23:13:47 +0100

    Getting ready for BCrypt

--- src/app/user/shared/api.service.ts
+++ src/app/user/shared/api.service.ts
@@ -1,12 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/model/user';
 
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
+
+    login(): User {
+
+        //Get salt from username
+        //Hasg the password using the salt
+        //Send the hashed password with username to server
+        //Get response
+
+        return { id: "", name: "", phoneNumber: "", username: "" }
+    }
 }
