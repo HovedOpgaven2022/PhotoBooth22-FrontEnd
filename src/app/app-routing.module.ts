2e44b51f14a724803e2e32fdc05f0416ea405e7e
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2022-12-19 11:37:33 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import {FaqComponent} from "./faq/faq.component";
 import {CarouselComponent} from "./carousel/carousel.component";
 import {AboutComponent} from "./about/about.component";
 import {ContactComponent} from "./contact/contact.component";
+import {NotfoundComponent} from "./notfound/notfound.component";
 const routes: Routes = [
   {path:'', component:CarouselComponent},
   {path:'home', component:CarouselComponent},
   {path:'about', component:AboutComponent},
   {path:'faq', component:FaqComponent},
-  {path:'contact', component:ContactComponent}
+  {path:'contact', component:ContactComponent},
+  {path: '404', component: NotfoundComponent},
+  {path: '**', redirectTo: '/404'}
 ]
 
 @NgModule({
